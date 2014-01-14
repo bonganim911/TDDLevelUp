1ea9501bca6dfaa05a195bf1c049dfb188fecf63
BonganiMthembu <bongani.mthembu@thoughtworks.com>
2014-01-15 00:02:49 +0200

    Finished Dojo Kata, Fizz Buzz problem.

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,12 +1,25 @@
 public class FizzBuzz {
-    public String result(int input) {
-        if (input % 3 == 0){
+    private int number;
+    public FizzBuzz(int number) {
+        this.number = number;
+    }
+
+    public String print() {
+        if (isFizz(number) && isBuzz(number)) {
+            return "FizzBuzz";
+        } else if (isFizz(number)) {
             return "Fizz";
-        } else if (input % 5 == 0) {
+        } else if (isBuzz(number)) {
             return "Buzz";
         }
-        return String.valueOf(input);
+        return String.valueOf(number);
+    }
 
+    private boolean isBuzz(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isFizz(int number) {
+        return number % 3 == 0;
     }
 }
-
--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -3,45 +3,61 @@ import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
 
 public class FizzBuzzTest {
-
     @Test
-    public void shouldKnowThatAThreeIsAFizz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String expected = "Fizz";
-        String actual = fizzBuzz.result(3);
-        assertEquals(expected, actual);
+    public void shouldKnowThatThreeIsAFizz(){
+        FizzBuzz fizzBuzz =  new FizzBuzz(3);
+        String actual = fizzBuzz.print();
+        assertEquals("Fizz", actual);
     }
 
     @Test
-    public void shouldKnowThatANumberDivisibleByThreeIsAFizz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String expected = "Fizz";
-        String actual = fizzBuzz.result(9);
-        assertEquals(expected, actual);
+    public void ShouldKnowThatOneIsAOne() {
+        FizzBuzz fizzBuzz =  new FizzBuzz(1);
+        String actual = fizzBuzz.print();
+        assertEquals("1", actual);
     }
 
     @Test
-    public void shouldKnowThatAFiveIsABuzz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String expected = "Buzz";
-        String actual = fizzBuzz.result(5);
-        assertEquals(expected, actual);
+    public void ShouldKnowThatAnyNumberNotADivisionOf3And5IsANumber() {
+        FizzBuzz fizzBuzz =  new FizzBuzz(4);
+        String actual = fizzBuzz.print();
+        assertEquals("4", actual);
     }
 
     @Test
-    public void shouldKnowThatANumberDivisibleByFiveIsAFizz(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String expected = "Buzz";
-        String actual = fizzBuzz.result(10);
-        assertEquals(expected, actual);
+    public void ShouldKnowThatADivisionOf3IsAFizz() {
+        FizzBuzz fizzBuzz =  new FizzBuzz(6);
+        String actual = fizzBuzz.print();
+        assertEquals("Fizz", actual);
     }
 
     @Test
-    public void shouldKnowThatANumberNotDivisibleByThreeOrFiveDoesNotChange(){
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        String expected = "8";
-        String actual = fizzBuzz.result(8);
-        assertEquals(expected, actual);
+    public void ShouldKnowThat5IsABuzz() {
+        FizzBuzz fizzBuzz =  new FizzBuzz(5);
+        String actual = fizzBuzz.print();
+        assertEquals("Buzz", actual);
     }
+
+    @Test
+    public void ShouldKnowThatADivisionOf5IsABuzz() {
+        FizzBuzz fizzBuzz =  new FizzBuzz(10);
+        String actual = fizzBuzz.print();
+        assertEquals("Buzz", actual);
+    }
+
+    @Test
+    public void ShouldKnowThat15IsAFizzBuzz() {
+        FizzBuzz fizzBuzz =  new FizzBuzz(15);
+        String actual = fizzBuzz.print();
+        assertEquals("FizzBuzz", actual);
+    }
+
+    @Test
+    public void ShouldKnowThatADivisionOfFiveAndThreeIsAFizzBuzz() {
+        FizzBuzz fizzBuzz =  new FizzBuzz(30);
+        String actual = fizzBuzz.print();
+        assertEquals("FizzBuzz", actual);
+    }
+
 
 }
